4fe90219a111070bbc64ef2afaa9dffcf2f0f738
Oksana Semonenko <ok.semonenko@gmail.com>
2016-08-22 17:39:19 +0300

    Задает функции для поля ввода и кнопки-крестика: если введён хотя бы один символ, появляется кнопка ×, при нажатии на которую введённый текст очищается

--- js/main.js
+++ js/main.js
@@ -1,1 +1,37 @@
 'use strict';
+
+(function () {
+  var searchForm = document.getElementById('search');
+  var searchField = searchForm.querySelector('#field');
+  var deleteBtn = searchForm.querySelector('#delete');
+
+  function hideDeleteBtn () {
+    deleteBtn.classList.remove('delete-btn--show');
+  }
+
+  function showDeleteBtn () {
+    deleteBtn.classList.add('delete-btn--show');
+  }
+
+  function deleteSearchFieldValue () {
+    searchField.value = '';
+  }
+
+  /*Показывает кнопку-крестик, если в поле введен хотя бы один символ
+  ** и прячет кнопку-крестик, если поле пустое
+   */
+  searchField.addEventListener('keyup', function () {
+    showDeleteBtn();
+    if (searchField.value === '') {
+      hideDeleteBtn();
+    }
+  });
+
+  /*Очищает поле ввода при нажатии на кнопку-крестик
+  **и прячет саму кнопку-крестик
+   */
+  deleteBtn.addEventListener('click', function () {
+    deleteSearchFieldValue();
+    hideDeleteBtn();
+  })
+})();
